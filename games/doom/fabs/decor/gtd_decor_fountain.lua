2b893063da5545128d64c4d46dd942de71f3dad8
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2025-07-16 16:33:10 +0800

    Reminder to conserve water during dry season.

---
+++ games/doom/fabs/decor/gtd_decor_fountain.lua
@@ -0,0 +1,14 @@
+PREFABS.Decor_gtd_fountain_something_1 =
+{
+  file   = "decor/gtd_decor_fountain.wad",
+  map    = "MAP01",
+
+  theme  = "!tech",
+
+  prob   = 3000,
+
+  where  = "point",
+  size   = 128,
+
+  bound_z1 = 0,
+}
