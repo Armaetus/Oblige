b668183166847dd2f9ba144d2aec832a9990866b
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-04-25 17:58:40 +0800

    New item closet - simple and universal with switch-opened version.

---
+++ games/doom/fabs/item/gtd_item_closet_simple.lua
@@ -0,0 +1,22 @@
+PREFABS.Item_gtd_closet_simple_1 =
+{
+  file   = "item/gtd_item_closet_simple.wad",
+  map    = "MAP01",
+
+  prob   = 75,
+
+  where  = "seeds",
+  seed_w = 2,
+  seed_h = 1,
+
+  deep = 16,
+
+  x_fit = "frame",
+  y_fit = "top",
+}
+
+PREFABS.Item_gtd_closet_simple_2 =
+{
+  template = "Item_gtd_closet_simple_1",
+  map = "MAP02"
+}
