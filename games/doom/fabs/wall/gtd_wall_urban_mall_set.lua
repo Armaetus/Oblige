03468bb938dea0230dbd36880cc02329ea702ea2
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-05-24 14:12:13 +0800

    Added new walls for mall prefab set.

---
+++ games/doom/fabs/wall/gtd_wall_urban_mall_set.lua
@@ -0,0 +1,26 @@
+PREFABS.Wall_gtd_mall_1 =
+{
+  file = "wall/gtd_wall_urban_mall_set.wad",
+  map = "MAP01",
+
+  prob = 50,
+
+  where = "edge",
+  height = 96,
+
+  group = "gtd_mall",
+
+  deep = 16,
+
+  bound_z1 = 0,
+  bound_z2 = 96,
+
+  x_fit = "frame",
+  z_fit = "top"
+}
+
+PREFABS.Wall_gtd_urban_mall_2 =
+{
+  template = "Wall_gtd_mall_1",
+  map = "MAP02"
+}
