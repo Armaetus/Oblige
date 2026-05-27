43b7e7724fbb95af9ffeb79f46e12d6531eac396
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-05-27 12:40:40 +0800

    Added generic door-styled keyless L-shape joiner.

---
+++ games/doom/fabs/joiner/curve_door.lua
@@ -0,0 +1,74 @@
+PREFABS.Joiner_curve_door_tech =
+{
+  file = "joiner/curve_door.wad",
+  map = "MAP01",
+  where = "seeds",
+
+  style = "doors",
+  theme = "tech",
+
+  shape = "L",
+  seed_h = 2,
+  seed_w = 2,
+
+  tex_BIGDOOR1 =
+  {
+    BIGDOOR1 = 1,
+    BIGDOOR2 = 1,
+    BIGDOOR3 = 1,
+    BIGDOOR4 = 1,
+    SPCDOOR3 = 1
+  }
+}
+
+PREFABS.Joiner_curve_door_urban =
+{
+  file = "joiner/curve_door.wad",
+  map = "MAP01",
+  where = "seeds",
+
+  style = "doors",
+  theme = "urban",
+
+  shape = "L",
+  seed_h = 2,
+  seed_w = 2,
+
+  tex_BIGDOOR1 =
+  {
+    BIGDOOR1 = 1,
+    BIGDOOR2 = 1,
+    BIGDOOR3 = 1,
+    BIGDOOR4 = 1,
+    SPCDOOR3 = 1,
+
+    WOODMET1 = 1,
+    WOODMET2 = 1,
+    BIGDOOR5 = 1,
+    BIGDOOR6 = 1,
+    BIGDOOR7 = 1
+  }
+}
+
+PREFABS.Joiner_curve_door_hell =
+{
+  file = "joiner/curve_door.wad",
+  map = "MAP01",
+  where = "seeds",
+
+  style = "doors",
+  theme = "hell",
+
+  shape = "L",
+  seed_h = 2,
+  seed_w = 2,
+
+  tex_BIGDOOR1 =
+  {
+    WOODMET1 = 1,
+    WOODMET2 = 1,
+    BIGDOOR5 = 1,
+    BIGDOOR6 = 1,
+    BIGDOOR7 = 1
+  }
+}
