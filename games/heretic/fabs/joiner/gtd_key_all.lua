40b5c57dc0ffe7870ac5f6da36b622d38b3487ec
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-05-28 13:20:57 +0800

    Added generic Boom-compat compact triple-key-locked door for Heretic.

---
+++ games/heretic/fabs/joiner/gtd_key_all.lua
@@ -0,0 +1,22 @@
+PREFABS.Locked_joiner_ALL =
+{
+  file   = "joiner/gtd_key_all.wad",
+  map    = "MAP01",
+
+  engine = "boom",
+
+  prob   = 100,
+
+  key    = "k_ALL",
+  where  = "seeds",
+  shape  = "I",
+
+  seed_w = 2,
+  seed_h = 1,
+
+  deep   = 16,
+  over   = 16,
+
+  x_fit  = "frame",
+  y_fit  = "frame",
+}
