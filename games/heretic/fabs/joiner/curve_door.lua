d2143fc4845895b144f3b16e8de9e0940bb8acd5
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-06-01 00:19:42 +0800

    Added generic curve_door fab to Heretic.

---
+++ games/heretic/fabs/joiner/curve_door.lua
@@ -0,0 +1,15 @@
+PREFABS.Joiner_curve_door_heretic =
+{
+  file = "joiner/curve_door.wad",
+  map = "MAP01",
+  where = "seeds",
+
+  style = "doors",
+  theme = "any",
+
+  prob = 100,
+
+  shape = "L",
+  seed_h = 2,
+  seed_w = 2,
+}
