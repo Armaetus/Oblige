395463b83e8b5817767412e01ad1ac1ebbc9027c
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-06-03 11:29:00 +0800

    New generalized nature props.

---
+++ games/doom/fabs/decor/gtd_decor_nature_props.lua
@@ -0,0 +1,51 @@
+PREFABS.Decor_gtd_nature_props_1 =
+{
+  file = "decor/gtd_decor_nature_props.wad",
+  map = "MAP01",
+
+  prob = 5000,
+
+  group = "natural_walls",
+
+  where = "point",
+  size = 64,
+  height = 64,
+
+  bound_z1 = 0,
+  bound_z2 = 64,
+
+  z_fit = "top"
+}
+
+PREFABS.Decor_gtd_nature_props_2 =
+{
+  template = "Decor_gtd_nature_props_1",
+  map = "MAP02"
+}
+
+PREFABS.Decor_gtd_nature_props_3 =
+{
+  template = "Decor_gtd_nature_props_1",
+  map = "MAP03",
+
+  height = 25,
+  bound_z2 = 25
+}
+
+PREFABS.Decor_gtd_nature_props_4 =
+{
+  template = "Decor_gtd_nature_props_1",
+  map = "MAP04",
+
+  height = 25,
+  bound_z2 = 25
+}
+
+PREFABS.Decor_gtd_nature_props_5 =
+{
+  template = "Decor_gtd_nature_props_1",
+  map = "MAP05",
+
+  height = 49,
+  bound_z2 = 49
+}
