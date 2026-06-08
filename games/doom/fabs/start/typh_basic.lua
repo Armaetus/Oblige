01763fc5b78e860ae5fead18cc0d94a56c1b3506
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-06-08 15:50:39 +0800

    Added size requirement to some start prefabs.

--- games/doom/fabs/start/typh_basic.lua
+++ games/doom/fabs/start/typh_basic.lua
@@ -2,18 +2,21 @@ PREFABS.Start_typh_basic =
 {
   file   = "start/typh_basic.wad",
   map   = "MAP01",
-  
+
   theme = "!hell",
 
   prob   = 30,
-
   where  = "point",
+
+  size = 80
 }
 
 PREFABS.Start_typh_basic_wide =
 {
   template = "Start_typh_basic",
   map   = "MAP02",
-  
+
   theme = "!hell",
+
+  size = 144
 }
