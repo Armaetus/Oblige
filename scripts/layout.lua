be13908ba712c8ccbd86aa60f440107caddcda5e
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-06-12 22:56:29 +0800

    Fix for room volume as extra odds for wall groups code being in the wrong spot.

--- scripts/layout.lua
+++ scripts/layout.lua
@@ -2105,14 +2105,15 @@ stderrf("Cages in %s [%s pressure] --> any_prob=%d  per_prob=%d\n",
     prob = math.clamp(0, prob, 100)
 
     for _,fg in pairs(R.floor_groups) do
+
+      -- increase odds of grouped walls for floor groups with larger volumes
+      if fg.volume and fg.volume > 64 then
+        prob = math.clamp(0, prob + (fg.volume/16),100)
+      end
+
       if rand.odds(prob) then
-
-        -- increase odds of grouped walls for floor groups with larger volumes
-        if fg.volume and fg.volume > 64 then
-          prob = math.clamp(0, prob + (fg.volume/16),100)
-        end
-
         fg.wall_group = rand.key_by_probs(tab)
+
         if not PARAM.bool_avoid_wall_group_reuse
         or (PARAM.bool_avoid_wall_group_reuse
         and PARAM.bool_avoid_wall_group_reuse == 1) then
