c1ad32660bb58c8d226adbdfb46dc9ae0952636e
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-06-29 20:53:53 +0800

    Openness and height no longer outright disable ceiling sinks. Openness and volume will instead be used as a probability score per ceil group. (higher score = better chance of ceil sink)

--- scripts/layout.lua
+++ scripts/layout.lua
@@ -2257,10 +2257,10 @@ stderrf("Cages in %s [%s pressure] --> any_prob=%d  per_prob=%d\n",
     if R.is_cave then return end
 
     for _,cg in pairs(R.ceil_groups) do
-      if cg.openness < 0.4 then goto skip end
+      --[[if cg.openness < 0.4 then goto skip end
 
       local height = cg.h - cg.max_floor_h
-      if height < 128 then goto skip end
+      if height < 128 then goto skip end]]
 
       local tab = grab_usable_sinks(R, cg, "ceiling")
       if tab == nil then return end
@@ -2270,8 +2270,10 @@ stderrf("Cages in %s [%s pressure] --> any_prob=%d  per_prob=%d\n",
 
       -- PLAIN keyword for sinks should now be ignored in
       -- favor of this direct prob
-      local ceil_sink_prob_add = math.remap_range(cg.volume, 16, 128, 0, 35)
-      if not rand.odds(40 + ceil_sink_prob_add) then name = "PLAIN" end
+      local volume_prob_add = math.remap_range(cg.volume, 16, 128, 0, 30, "clamp_it")
+      local openness_prob_add = math.remap_range(cg.openness, 0.2, 0.6, 0, 30, "clamp_it")
+      local has_ceil_sink = rand.odds(30 + volume_prob_add + openness_prob_add)
+      if has_ceil_sink == false then name = "PLAIN" end
 
       if name ~= "PLAIN" then
         cg.sink = GAME.SINKS[name]
@@ -2284,7 +2286,6 @@ stderrf("Cages in %s [%s pressure] --> any_prob=%d  per_prob=%d\n",
           end
         end
       end
-      ::skip::
     end
 
 
@@ -2298,7 +2299,7 @@ stderrf("Cages in %s [%s pressure] --> any_prob=%d  per_prob=%d\n",
     -- PLAIN keyword for sinks should now be ignored in
     -- favor of this direct prob
     if not R.liquid_sink_prob then
-      R.liquid_sink_prob = rand.odds(65 + math.remap_range(R.svolume, 8, 64, 0, 30))
+      R.liquid_sink_prob = rand.odds(65 + math.remap_range(R.svolume, 8, 64, 0, 30, "clamp_it"))
     end
     if R.liquid_sink_prob == false then name = "PLAIN" end
 
