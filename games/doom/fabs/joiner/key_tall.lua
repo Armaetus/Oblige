f78631d350618f0b4c7045b04f4e0af10611f449
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-07-23 14:44:30 +0800

    Redid some visuals on key_tall joiners and converted entries to templates, removed duplicate maps.

--- games/doom/fabs/joiner/key_tall.lua
+++ games/doom/fabs/joiner/key_tall.lua
@@ -23,47 +23,20 @@ PREFABS.Locked_joiner_key1_tall =
   y_fit  = "frame",
 }
 
-
 PREFABS.Locked_joiner_key2_tall =
 {
-  file   = "joiner/key_tall.wad",
-  map    = "MAP02",
-
-  prob   = 100,
+  template = "Locked_joiner_key1_tall",
 
   key    = "k_yellow",
-  where  = "seeds",
-  shape  = "I",
 
-  seed_w = 1,
-  seed_h = 1,
-
-  deep   = 16,
-  over   = 16,
-
-  x_fit  = "frame",
-  y_fit  = "frame",
+  line_33 = 34
 }
-
 
 PREFABS.Locked_joiner_key3_tall =
 {
-  file   = "joiner/key_tall.wad",
-  map    = "MAP03",
-
-  prob   = 100,
+  template = "Locked_joiner_key1_tall",
 
   key    = "k_blue",
-  where  = "seeds",
-  shape  = "I",
 
-  seed_w = 1,
-  seed_h = 1,
-
-  deep   = 16,
-  over   = 16,
-
-  x_fit  = "frame",
-  y_fit  = "frame",
+  line_33 = 32
 }
-
