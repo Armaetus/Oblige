8ea69bdb08060e409c3b21744610d39fb7b9565e
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-08-03 21:13:37 +0800

    Added 2 new nature-only item fabs.

--- games/doom/fabs/item/gtd_item_natural.lua
+++ games/doom/fabs/item/gtd_item_natural.lua
@@ -3,7 +3,7 @@ PREFABS.Item_natural_closet_1 =
   file  = "item/gtd_item_natural.wad",
   map = "MAP01",
 
-  prob = 50,
+  prob = 350,
 
   where  = "seeds",
   seed_w = 2,
@@ -16,11 +16,35 @@ PREFABS.Item_natural_closet_1 =
   deep = 16,
 
   bound_z1 = 0,
-  bound_z2 = 96
+  bound_z2 = 96,
+
+  z_fit = "top"
 }
 
 PREFABS.Item_natural_closet_2 =
 {
   template = "Item_natural_closet_1",
-  map = "MAP02"
+  map = "MAP02",
+}
+
+PREFABS.Item_natural_closet_3 =
+{
+  template = "Item_natural_closet_1",
+  map = "MAP03",
+
+  height = 128,
+
+  bound_z2 = 128
+}
+
+PREFABS.Item_natural_closet_4 =
+{
+  template = "Item_natural_closet_1",
+  map = "MAP04",
+
+  height = 128,
+
+  bound_z2 = 128,
+
+  z_fit = { 118,120 , 122,124 }
 }
