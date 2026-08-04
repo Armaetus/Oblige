0c0d866efe4eabd1928fa2cd94d3f33943869327
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-08-04 17:33:05 +0800

    Added Start_gtd_armaetus_sky_floor variation as peered start for Exit_armaetus_sky_floor_exit.

---
+++ games/doom/fabs/start/gtd_armaetus_sky_floor_start.lua
@@ -0,0 +1,17 @@
+PREFABS.Start_gtd_armaetus_sky_floor =
+{
+  file   = "start/gtd_armaetus_sky_floor_start.wad",
+  map    = "MAP01",
+
+  prob   = 300,
+
+  where  = "seeds",
+
+  seed_w = 2,
+  seed_h = 2,
+
+  deep   = 16,
+
+  x_fit  = "frame",
+  y_fit  = "top",
+}
