a19c78744dce4e56b6bd06fb6b8f46849503d787
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-08-16 01:18:08 +0800

    HN Debug Info: Changes to debug area printouts: now shows areas with neighboring ceilings that don't have the same texture, as it it should preferrably always be different.

--- modules/modded_game_extras.lua
+++ modules/modded_game_extras.lua
@@ -1973,38 +1973,43 @@ function MODDED_GAME_EXTRAS.create_hn_info(self, LEVEL)
     info.editor_num = PARAM.hn_thing_start_offset
 
     for _,A in pairs(R.areas) do
-      if A.mode == "floor" and A.dead_end or (A.mode == "floor" and A.is_porch and A.room.is_outdoor) then
-        info.name = "AREA_" .. A.id .. " (" .. A.fm_history .. ")"
-        info.editor_num = PARAM.hn_thing_start_offset
+      for _,N in pairs(A.neighbors) do
+        if (A.ceil_h == N.ceil_h and A.ceil_mat ~= N.ceil_mat)
+        or (A.floor_h == A.floor_h and A.floor_mat ~= A.floor_mat)
+        and A.room == N.room then
+          if A.mode == "floor" and not A.mode.chunk then
+            info.name = "AREA_" .. A.id .. " (" .. A.fm_history .. ")"
+            info.editor_num = PARAM.hn_thing_start_offset
 
-        if SCRIPTS.hn_id_table[info.name] then
-          info.editor_num = SCRIPTS.hn_id_table[info.name].id
-        elseif not SCRIPTS.hn_id_table[info.name] then
-          SCRIPTS.hn_id_table[info.name] = {}
-          SCRIPTS.hn_id_table[info.name].id = info.editor_num
-          SCRIPTS.hn_id_table[info.name].name = info.name
-          info.editor_num = PARAM.hn_thing_start_offset
-          PARAM.hn_thing_start_offset = PARAM.hn_thing_start_offset + 1
-        end
+            if SCRIPTS.hn_id_table[info.name] then
+              info.editor_num = SCRIPTS.hn_id_table[info.name].id
+            elseif not SCRIPTS.hn_id_table[info.name] then
+              SCRIPTS.hn_id_table[info.name] = {}
+              SCRIPTS.hn_id_table[info.name].id = info.editor_num
+              SCRIPTS.hn_id_table[info.name].name = info.name
+              info.editor_num = PARAM.hn_thing_start_offset
+              PARAM.hn_thing_start_offset = PARAM.hn_thing_start_offset + 1
+            end
 
-        local S = A.seeds[1]
-        for _,S2 in pairs(A.seeds) do
-          if A.mode and A.mode == "floor" then
-            S = S2
-            break;
+            local S = A.seeds[1]
+            for _,S2 in pairs(A.seeds) do
+              if A.mode and A.mode == "floor" then
+                S = S2
+                break;
+              end
+            end
+            hn_add_entity(info, S.mid_x, S.mid_y, A.floor_h + 1)
+
+            local e = {}
+            e.id = 9029
+            e.x = S.mid_x
+            e.y = S.mid_y
+            e.z = A.floor_h + 1
+
+            gui.printf("AREA tracker placed in: " .. S.mid_x .. ", " .. S.mid_y .. "\n")
+            raw_add_entity(e)
           end
         end
-        hn_add_entity(info, S.mid_x, S.mid_y, A.floor_h + 1)
-
-        local e = {}
-        e.id = 9029
-        e.name = "black_particle_fountain"
-        e.x = S.mid_x
-        e.y = S.mid_y
-        e.z = A.floor_h + 1
-
-        gui.printf("AREA tracker placed in: " .. S.mid_x .. ", " .. S.mid_y .. "\n")
-        raw_add_entity(e)
       end
     end
 
