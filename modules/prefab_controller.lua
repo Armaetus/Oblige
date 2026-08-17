5ffe1965e4bebad67e4e89e6bf0c3ab42f9f098e
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-08-18 01:30:14 +0800

    Redid some probs for new wall groups in Prefab Controller's Test New Content.

--- modules/prefab_controller.lua
+++ modules/prefab_controller.lua
@@ -149,11 +149,11 @@ function PREFAB_CONTROL.fine_tune_filters()
   }
   local new_fab_groups_multipliers =
   {
-    gtd_wall_quakish_insets_horizon = 4,
-    gtd_wall_quakish_insets_horizon_mid = 4,
-    gtd_wall_quakish_insets_horizon_top = 4,
-    gtd_ribbed_lights_ribbed_steps = 4,
-    gtd_generic_artsy_corpo_art = 8
+    gtd_wall_quakish_insets_horizon = 2,
+    gtd_wall_quakish_insets_horizon_mid = 2,
+    gtd_wall_quakish_insets_horizon_top = 2,
+    gtd_ribbed_lights_ribbed_steps = 3,
+    gtd_generic_artsy_corpo_art = 6
   }
   local new_themes_multipliers =
   {
