abddfa77f29e1b4e2e2b0424309312e11f82d22b
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-09-01 01:48:46 +0800

    More prefab additions and fixes.

---
+++ games/doom/fabs/decor/gtd_decor_tech_grated_machines.lua
@@ -0,0 +1,27 @@
+PREFABS.Decor_wall_grated_machines_pillar =
+{
+  file = "decor/gtd_decor_tech_grated_machines.wad",
+  map = "MAP01",
+
+  where = "point",
+
+  group = "gtd_wall_grated_machines_damaged_wall",
+
+  prob = 5000,
+
+  size = 64,
+  height = 96,
+
+  bound_z1 = 0,
+  bound_z2 = 96,
+
+  z_fit = "top"
+}
+
+PREFABS.Decor_wall_grated_machines_pillar_broken =
+{
+  template = "Decor_wall_grated_machines_pillar",
+  map = "MAP02",
+
+  prob = 1000
+}
