9d983672262c1e4244c4e07ee30c692b6795f58c
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-09-03 15:17:59 +0800

    Redid fit settings on new prefab set.

--- games/doom/fabs/decor/gtd_decor_tech_grated_machines.lua
+++ games/doom/fabs/decor/gtd_decor_tech_grated_machines.lua
@@ -20,22 +20,28 @@ PREFABS.Decor_wall_grated_machines_pillar =
 
 PREFABS.Decor_wall_grated_machines_pillar_broken =
 {
-  template = "Decor_wall_grated_machines_pillar",
+  file = "decor/gtd_decor_tech_grated_machines.wad",
   map = "MAP02",
+
+  where = "point",
+
+  group = "gtd_wall_grated_machines_damaged_wall",
 
   prob = 1000,
 
-  z_fit = "nil"
+  size = 64,
+  height = 96,
+
+  bound_z1 = 0,
+  bound_z2 = 96
 }
 
 PREFABS.Decor_wall_grated_machines_pillar_large_broken =
 {
-  template = "Decor_wall_grated_machines_pillar",
+  template = "Decor_wall_grated_machines_pillar_broken",
   map = "MAP03",
 
   prob = 7500,
 
   size = 96,
-
-  z_fit = "nil"
 }
