610355263ae99cb3ee34a4aeeda091e948f87109
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-09-22 01:52:51 +0800

    Fixed pillars not reaching all the way to floor level in 3D floor stair fabs.

--- games/doom/fabs/stairs/gtd_steepnesses_128_3d_floor.lua
+++ games/doom/fabs/stairs/gtd_steepnesses_128_3d_floor.lua
@@ -51,7 +51,7 @@ PREFABS.Stair_gtd_ladder_128_3d =
   delta_h = 128
 }
 
-PREFABS.Stair_gtd_teleporterct_128_3d =
+PREFABS.Stair_gtd_teleporter_128_3d =
 {
   template = "Stair_gtd_lift_128_3d",
   map = "MAP04",
