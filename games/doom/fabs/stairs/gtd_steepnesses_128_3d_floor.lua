4acb7d4d102f51f3cf80a84a17152c8ddc0d016d
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-09-21 21:41:28 +0800

    Updated 3D floor fabs with more variants and better texturing.

--- games/doom/fabs/stairs/gtd_steepnesses_128_3d_floor.lua
+++ games/doom/fabs/stairs/gtd_steepnesses_128_3d_floor.lua
@@ -15,8 +15,8 @@ PREFABS.Stair_gtd_lift_128_3d =
   seed_w = 1,
   seed_h = 1,
 
-  x_fit = { 56,72 },
-  y_fit = { 20,24 , 80,84 },
+  x_fit = { 28,32 , 64,68 , 96,100 },
+  y_fit = { 20,24 , 64,68 },
 
   bound_z1 = 0,
 
@@ -28,10 +28,36 @@ PREFABS.Stair_gtd_stairs_128_3d =
   template = "Stair_gtd_lift_128_3d",
   map = "MAP02",
 
-  x_fit = { 56,72 },
-  y_fit = { 20,24 , 100,104 },
+  x_fit = { 28,32 , 64,68 , 96,100 },
+  y_fit = { 20,24 },
 
   seed_h = 2,
+
+  bound_z1 = 0,
+
+  delta_h = 128
+}
+
+PREFABS.Stair_gtd_ladder_128_3d =
+{
+  template = "Stair_gtd_lift_128_3d",
+  map = "MAP03",
+
+  x_fit = { 28,32 , 64,68 , 96,100 },
+  y_fit = { 20,24 , 64,68 },
+
+  bound_z1 = 0,
+
+  delta_h = 128
+}
+
+PREFABS.Stair_gtd_teleporterct_128_3d =
+{
+  template = "Stair_gtd_lift_128_3d",
+  map = "MAP03",
+
+  x_fit = { 28,32 , 96,100 },
+  y_fit = { 20,24 },
 
   bound_z1 = 0,
 
