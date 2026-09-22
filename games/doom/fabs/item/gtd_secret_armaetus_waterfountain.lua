a0218791adaeec8645b9e3cdcfe916348ef44ba0
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-09-22 17:25:14 +0800

    New secret version of waterfountain pic fab.

---
+++ games/doom/fabs/item/gtd_secret_armaetus_waterfountain.lua
@@ -0,0 +1,24 @@
+PREFABS.Item_gtd_secret_version_of_armaetus_waterfountain =
+{
+  file = "item/gtd_secret_armaetus_waterfountain.wad",
+  map = "MAP01",
+  where = "seeds",
+
+  liquid = true,
+
+  prob  = 50,
+
+  theme = "!tech",
+
+  key = "secret",
+
+  seed_w = 2,
+  seed_h = 2,
+
+  height = 128,
+
+  deep = 16,
+
+  x_fit = "frame",
+  y_fit = "top"
+}
