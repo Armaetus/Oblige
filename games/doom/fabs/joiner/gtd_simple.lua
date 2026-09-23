53a7a22c27533c62b4ce91f1041b2113e995495e
MsrSgtShooterPerson <gtdcarthage@gmail.com>
2026-09-23 13:48:28 +0800

    New simple joiner.

---
+++ games/doom/fabs/joiner/gtd_simple.lua
@@ -0,0 +1,19 @@
+PREFABS.Joiner_gtd_triple_lite3 =
+{
+  file   = "joiner/gtd_simple.wad",
+  map    = "MAP01",
+
+  prob   = 1000,
+
+  where  = "seeds",
+  shape  = "I",
+
+  seed_w = 1,
+  seed_h = 2,
+
+  deep   = 16,
+  over   = 16,
+
+  x_fit = { 60,68 },
+  y_fit = { 56,64 , 96,104 },
+}
